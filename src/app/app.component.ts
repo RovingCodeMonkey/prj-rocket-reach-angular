dc470b3273e46b8280f9c3ff7bd2e5d6b90192ce
Anthony Roberts <aroberts@rovingcodemonkey.io>
2020-06-24 12:35:19 -0700

    adding some error feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,34 @@ import { TumblerApiService } from './tumbler-api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rocket-reach';
   posts = [];
-  tumblrSubdomain: string; 
+  tumblrSubdomain: string;
+  error: string;
+  genericError = 'An unexpected error has occurred';
 
-  constructor(private tumblerApi: TumblerApiService) { }
+  constructor(private tumblerApi: TumblerApiService) {}
 
   loadBlog(value: string) {
-    this.tumblerApi.get(value)
-    .subscribe((response: any) => {
-      if(response?.posts) {
-        this.posts = response.posts;
-      } else
-      {
+    this.tumblerApi.get(value).subscribe(
+      (response: any) => {
+        if (response?.posts) {
+          this.posts = response.posts;
+          this.error = '';
+        } else {
+          this.posts = [];
+          this.error = this.genericError;
+        }
+      },
+      (err) => {
         this.posts = [];
+        if (err?.error?.error) {
+          this.error = err?.error?.error ? err.error.error : this.genericError;
+        }
       }
-    });
+    );
   }
 }
--- src/app/tumbler-api.service.ts
+++ src/app/tumbler-api.service.ts
@@ -1,17 +1,15 @@
-import { Injectable } from '@angular/core';import {
-  HttpClient
-} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TumblerApiService {
-
   // inject the HttpClient as http so we can use it in this class
   constructor(private http: HttpClient) {}
 
   // return what comes back from this http call
   get(subDomain: string) {
-    return this.http.jsonp(`https://${subDomain}.tumblr.com/api/read/json`, 'callback');
+    return this.http.get(`http://127.0.0.1:8080/tumblr/${subDomain}`);
   }
 }
